3d8602da96d270aa6581c8f60c122c08173e2d41
nayphyokyaw <nayphyokyaw@gmail.com>
2017-06-25 15:58:10 +0630

    GET todo and Post todo

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
 	// console.log(req.body);
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
+
 app.listen(port, () => {
 	console.log('Server is starting on Port: ' + port);
 });
+
+module.exports = {app};
